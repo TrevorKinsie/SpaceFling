38311961bc02145b39d40e23ac6f5ea8c591e54c
Trevor Kinsie <trevor.kinsie@gmail.com>
2018-01-08 08:00:25 -0500

    Deleted the comments

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -5,46 +5,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class SphereController : MonoBehaviour {//,  IBeginDragHandler, IDragHandler, IEndDragHandler {
-                                               /*
-                                               public static GameObject DraggedInstance;
-
-                                               Vector3 _startPosition;
-                                               Vector3 _offsetToMouse;
-                                               float _zDistanceToCamera;
-
-                                               #region Interface Implementations
-
-                                               public void OnBeginDrag(PointerEventData eventData)
-                                               {
-                                                   DraggedInstance = gameObject;
-                                                   _startPosition = transform.position;
-                                                   _zDistanceToCamera = Mathf.Abs(_startPosition.z - Camera.main.transform.position.z);
-
-                                                   _offsetToMouse = _startPosition - Camera.main.ScreenToWorldPoint(
-                                                       new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
-                                                   );
-                                               }
-
-                                               public void OnDrag(PointerEventData eventData)
-                                               {
-                                                   if (Input.touchCount > 1)
-                                                       return;
-
-                                                   transform.position = Camera.main.ScreenToWorldPoint(
-                                                       new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
-                                                       ) + _offsetToMouse;
-                                               }
-
-                                               public void OnEndDrag(PointerEventData eventData)
-                                               {
-                                                   DraggedInstance = null;
-                                                   _offsetToMouse = Vector3.zero;
-
-                                               }
-
-                                               #endregion
-                                               */
+public class SphereController : MonoBehaviour {
 
     private Vector3 screenPoint;
     private Vector3 offset;
